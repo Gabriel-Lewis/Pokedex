d065cb9ac17e285e39f75a6ccc62914a79c79c57
Gabriel Lewis <benbowgl1@gmail.com>
2016-01-16 17:19:25 -0500

    Added details to detailVC, updated info.plist to allow http requests, updated pokemon class with pokemon props, and added alamofire capability

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,6 +11,17 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
 	@IBOutlet weak var nameLbl: UILabel!
+	@IBOutlet weak var mainIMG: UIImageView!
+	@IBOutlet weak var descriptionLbl: UILabel!
+	@IBOutlet weak var typeLbl: UILabel!
+	@IBOutlet weak var defenseLbl: UILabel!
+	@IBOutlet weak var attackLbl: UILabel!
+	@IBOutlet weak var weightLbl: UILabel!
+	@IBOutlet weak var heightLbl: UILabel!
+	@IBOutlet weak var currentEvoImage: UIImageView!
+	@IBOutlet weak var nextEvoImage: UIImageView!
+	@IBOutlet weak var pokedexIDLbl: UILabel!
+	@IBOutlet weak var evoLabel: UILabel!
 	
 	var pokemon: Pokemon!
 	
@@ -18,5 +29,12 @@ class PokemonDetailVC: UIViewController {
 		super.viewDidLoad()
 		
 		nameLbl.text = pokemon.name
+		mainIMG.image = UIImage(named: "\(pokemon.pokedexID)")
+		
+		pokemon.downloadPokemonDetails { () -> () in
+		}
+	}
+	@IBAction func backBtnPressed(sender: AnyObject) {
+		dismissViewControllerAnimated(true, completion: nil)
 	}
 }
---
+++ Pokedex/constants.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  Pokedex
+//
+//  Created by Gabriel Benbow on 1/16/16.
+//  Copyright © 2016 Gabriel Benbow. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
